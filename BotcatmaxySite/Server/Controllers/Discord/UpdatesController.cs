fb48c6aaa14af933a3b04dd8491e0907e06983e8
RealSGII2 <rbxsgii2@gmail.com>
2020-08-27 17:43:15 -0700

    Add updates API

---
+++ BotcatmaxySite/Server/Controllers/Discord/UpdatesController.cs
@@ -0,0 +1,43 @@
+﻿using BotcatmaxySite.Shared.Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System.Text.Json;
+using System.Net;
+using BotcatmaxySite.Server.Discord;
+using Discord.WebSocket;
+using Discord;
+
+namespace BotcatmaxySite.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UpdatesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<UpdateMessage>> Get()
+        {
+            try
+            {
+                var messages = await DiscordData.client.GetGuild(285529027383525376)
+                    .GetTextChannel(700825496010621041)
+                    .GetMessagesAsync()
+                    .FlattenAsync();
+                return messages.Where(m => m.Source == MessageSource.User).Take(3).Select(m => new UpdateMessage
+                {
+                    Content = m.Content,
+                    Timestamp = m.Timestamp.DateTime
+                });
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw exception;
+            }
+        }
+    }
+}
---
+++ BotcatmaxySite/Shared/Discord/UpdateMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotcatmaxySite.Shared.Discord
+{
+    public class UpdateMessage
+    {
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
